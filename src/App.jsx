066c18ad9b876068cb464e67193fc10458a9da19
channykim00 <hadeskim0429@gmail.com>
2025-07-02 19:46:41 +0900

    feat: react router dom 추가 및 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/:uniqueUrl"
+          element={<Home />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 };
+
 export default App;
