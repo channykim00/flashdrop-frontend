fb13a6308fd900b69845db9dc552dfafa3b6ba6a
channykim00 <hadeskim0429@gmail.com>
2025-07-27 00:46:15 +0900

    chore: 서버 API_URL EC2 주소 반영

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://port-0-flashdrop-backend-md6dio3qdb6e915b.sel5.cloudtype.app";
+export const API_URL = "http://52.79.234.77:4000";
 
 export const FILE_TYPE_OPTIONS = [
   {
